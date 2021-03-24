06edcad55cb9e69983d8b14a597f24b1638d3ef0
Lukáš Varga <lukas.varga128@gmail.com>
2021-03-24 17:11:36 +0100

    labs fertig

---
+++ 03/fixing.hs
@@ -0,0 +1,29 @@
+a1:: Integer
+a1 = 5                   -- Hint  - What type is True
+
+a2 :: Integer
+a2 = 7 * 6         
+
+a3 :: Integer
+a3 = 99                  
+
+a4 :: Int
+a4 = length "4.0"         
+
+a6 :: String
+a6 = " "              
+
+a7 :: Bool
+a7 = if (a3==99) then True else False     
+
+a8 :: Char
+a8 = if (2<5) then 'a' else 'a'  
+
+a9 :: Int -> Int
+a9 x = x `div` 2
+
+a10 :: Int
+a10 = a9 a4
+
+a11 :: Bool
+a11 = True && True  
