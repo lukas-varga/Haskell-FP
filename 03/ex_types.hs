e0026ddb5b7c1234d98d389cb57b8c092e7769ba
Lukáš Varga <lukas.varga128@gmail.com>
2021-03-24 17:46:55 +0100

    saving unfinished exercise 2

---
+++ 03/ex_types.hs
@@ -0,0 +1,22 @@
+---------- Exercise 1 ----------
+a1 = ['a','b','c'] --[Char]
+a2 = ('a', 'b', 'c') --(Char,Char,Char)
+a3 = [(False, '0'), (True, '1')] --[(Bool,Char)]
+a4 = (['1', '0'], ['0', '1']) --([Char],[Char])
+a5 = [tail, init, reverse] --[Function]
+
+
+---------- Exercise 2 ----------
+bools :: [Bool]
+bools =  [True, True]
+
+nums :: [[Int]]
+nums = [[2,3,5,1],[2,3,5,1]]
+
+
+{-
+add :: Int -> Int -> Int-> Int
+add a = 5 + add a
+copy :: a -> (a,a)
+apply :: (a -> b) -> a -> b
+-}
