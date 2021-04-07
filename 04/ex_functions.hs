dc26877a5bff3911dfe5fbf871dc824ffde21539
Lukáš Varga <lukas.varga128@gmail.com>
2021-04-07 12:23:17 +0200

    Finished exercise 4 about funtions, clauses and pattern matching

--- 04/ex_functions.hs
+++ 04/ex_functions.hs
@@ -48,9 +48,74 @@ third xs =  if length xs >= 3 then head(tail(tail xs))
             else head xs
 
 third' :: [a] -> a
-third' xs =  if length xs >= 3 then xs !! 2
-            else head xs
+third' xs =     if length xs >= 3 then xs !! 2
+                else head xs
 
 third'' :: [a] -> a
 third'' (_:_:x:_) = x
+
+---------- Exercise 9 ----------
+safetail :: [a] -> [a]
+safetail xs =   if isFull then tail xs
+                else xs 
+        where
+        isFull = not (null xs)
+                
+safetail' :: [a] -> [a]
+safetail' xs    | isFull = tail xs
+                | otherwise = xs               
+        where
+        isFull = not (null xs)
+
+safetail'' :: [a] -> [a]
+safetail'' [] = []
+safetail'' (_:xs) = xs 
+
+---------- Exercise 10 ----------
+myOr :: Bool -> Bool -> Bool
+True `myOr` True = True
+True `myOr` False = True
+False `myOr` True = True
+False `myOr` False = False
+
+myOr' :: Bool -> Bool -> Bool
+False `myOr'` False = False
+_ `myOr'` _ = True
+
+myOr'' :: Bool -> Bool -> Bool
+False `myOr''` b = b
+True `myOr''` _ = True
+
+---------- Exercise 11 ----------
+lucky :: Integral a => a-> String
+lucky x | x == 7 = "Lucky you.. Proceed directly to buy a lottery ticket."
+        | x == 13 = "You, sadly are quite unlucky. Do not, under any circumstances, invest money today"
+        | otherwise = "Mmmm.... Can't really say...."
+
+---------- Exercise 12 ----------
+myFst :: (a,b,c) -> a
+myFst (x,_,_) = x
+mySnd :: (a,b,c) -> b
+mySnd (_,y,_) = y
+myThd :: (a,b,c) -> c
+myThd (_,_,z) = z
+
+---------- Exercise 13 ----------
+luhnDouble :: Int -> Int
+luhnDouble x
+        | doubleX > 9 = doubleX - 9
+        | otherwise = doubleX
+        where
+        doubleX = x*2
+
+luhn :: Int -> Int-> Int-> Int-> Bool
+luhn a b c d
+        | sum [luhnDouble a, b, luhnDouble c, d] `mod` 10 == 0 = True
+        | otherwise = False
+
+---------- Exercise 14 ----------
+luhnGetCheck:: Int -> Int-> Int-> Int
+luhnGetCheck a b c = calc
+        where 
+        calc = 10 - (sum [luhnDouble a, b, luhnDouble c] `mod` 10) 
 
