8eb7db95ed70b7faf02b10beda4a00dab55d636f
Lukáš Varga <lukas.varga128@gmail.com>
2021-06-04 19:52:45 +0200

    Half way there

--- Assingment/covid/app/Main.hs
+++ Assingment/covid/app/Main.hs
@@ -9,7 +9,72 @@ main :: IO ()
 main  = do
     (file:_) <- getArgs
     quotes <- readFile file
-    putStrLn quotes
+
+    putStrLn $ ""
+    putStrLn "Enter start and end date to know the average, highest and lowest number of new covid cases during that time"
+    putStrLn "Start (yyyy-mm-dd):"
+    startDateCases <- getLine
+    putStrLn "Start (yyyy-mm-dd):"
+    endDateCases <- getLine
+
+    putStrLn $ ""
+
+    putStrLn "Enter start and end date to know what are the highest and lowest number of vaccinations during that time"
+    putStrLn "Start (yyyy-mm-dd):"
+    startDateVacc <- getLine
+    putStrLn "Start (yyyy-mm-dd):"
+    endDateVacc <- getLine
+    putStrLn $ ""
+    
+    let avgNC = avgNewCases $ makeAllQuotes quotes
+    let lowNC = computeLowDate (makeAllQuotes quotes) New_cases
+    let highNC = computeHighDate (makeAllQuotes quotes) New_cases
+    putStrLn   "---------------------------------------------------------------"
+    putStrLn   "| New Cases (All Time)  |       Number      |       Date       "
+    putStrLn   "---------------------------------------------------------------"
+    putStrLn $ "| Average               | " ++ show (avgNC) ++ " | " ++ "---"
+    putStrLn $ "| Lowest Number         | " ++ show(new_cases lowNC) ++ "\t\t   " ++ " | " ++ show(day lowNC)
+    putStrLn $ "| Highest Number        | " ++ show(new_cases highNC) ++ "\t   " ++ " | " ++ show(day highNC)
+
+    putStrLn $ ""
+    putStrLn   ""
+    putStrLn $ ""
+
+    --let highRollNC = computeHighRollNC (makeAllQuotes quotes) New_cases
+    let highRollNC = computeHighRollNC $ makeAllQuotes quotes
+    putStrLn   "---------------------------------------------------------------"
+    putStrLn   "| New Cases Rolling 7d  |      Number                          "
+    putStrLn   "---------------------------------------------------------------"
+    putStrLn $ "| Highest number        | " ++ show(highRollNC) 
+
+    putStrLn $ ""
+    putStrLn   ""
+    putStrLn $ ""
+
+    let avgRangeNC = 
+    let lowRangeNC = 
+    let highRangeNC =
+    putStrLn   "---------------------------------------------------------------"
+    putStrLn $ "| Entered Range         | " ++ startDateCases ++ " - " ++ endDateCases
+    putStrLn   "| New Cases             |       Number      |     Days Diff    "
+    putStrLn   "---------------------------------------------------------------"
+    putStrLn $ "| Average               | " 
+    putStrLn $ "| Lowest Number         | "
+    putStrLn $ "| Highest Number        | "
+    
+    putStrLn $ ""
+    putStrLn   ""
+    putStrLn $ ""
+
+    let lowRangeVC = 
+    let highRangeVC =
+    putStrLn   "---------------------------------------------------------------"
+    putStrLn $ "| Entered Range         | " ++ startDateVacc ++ " - " ++ endDateVacc
+    putStrLn   "| Vaccinations          |       Number      |     Days Diff    "
+    putStrLn   "---------------------------------------------------------------"
+    putStrLn $ "| Lowest Number         | "
+    putStrLn $ "| Highest Number        | "
+    
 {-
     let (minQ, maxQ, numDays) = computeMinMaxDays  (makeAllQuotes quotes) Open
     let (minLow, maxLow, daysLow) = computeMinMaxDays (makeAllQuotes quotes) Low
--- Assingment/covid/src/StatReport.hs
+++ Assingment/covid/src/StatReport.hs
@@ -2,10 +2,52 @@ module StatReport where
 
 import Data.List (sortOn)
 import Data.Time (diffDays)
+import Data.List (tails)
+import Debug.Trace
 import QuoteData
 
-{-
+avgNewCases :: [QuoteData] -> Double --the average (new) covid cases over the entire period
+avgNewCases all = sum (map new_cases all) / fromIntegral (length all)
 
+computeLowDate :: [QuoteData] -> QField  -> QuoteData
+computeLowDate   all onfield  = lowNC where
+                      get = field2fun onfield
+                      sorted = sortOn get all
+                      lowNC = head sorted
+
+computeHighDate :: [QuoteData] -> QField  -> QuoteData
+computeHighDate   all onfield  = highNC where
+                      get = field2fun onfield
+                      sorted = sortOn get all
+                      highNC = last sorted
+
+computeHighRollNC:: [QuoteData] -> Double
+computeHighRollNC all = res where
+                    xs = map new_cases all
+                    res = maxRoll 7 xs
+
+maxRoll :: Int -> [Double] -> Double
+maxRoll p [a,b,c,d,e,f,g] = (a+b+c+d+e+f+g) / fromIntegral p
+maxRoll p xs = max (sum (take p xs) / fromIntegral p) (maxRoll p $ tail xs) 
+
+
+computeAvgRangeNewCases :: [QuoteData] -> String -> String
+computeAvgRangeNC = undefined
+
+computeAvgRangeNewCases :: [QuoteData] -> String -> String
+computeLowRangeNC = undefined
+
+computeAvgRangeNewCases :: [QuoteData] -> String -> String
+computeHighRangeNC = undefined
+
+
+computeAvgRangeNewCases :: [QuoteData] -> String -> String
+computeLowVC = undefined
+
+computeAvgRangeNewCases :: [QuoteData] -> String -> String
+computeHighVC = undefined
+
+{-
 avgOpen :: [QuoteData] -> Double  -- average  opening values
 avgOpen all =  sum (map open all)  / fromIntegral (length all)
 
