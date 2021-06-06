3bf45dd14207d2da8252e2620f3aa90cb5d1852c
Lukáš Varga <lukas.varga128@gmail.com>
2021-06-06 17:57:21 +0200

    Exercises done

--- 10-interactive-programs/explain/src/Lib.hs
+++ 10-interactive-programs/explain/src/Lib.hs
@@ -3,4 +3,19 @@ module Lib
     ) where
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+        putStrLn "n:"
+        n <- getLine 
+        let numN = read n :: Integer
+        getInteger numN []
+
+getInteger :: Integer -> [Integer] -> IO () 
+getInteger 0 arr = putStr $ show (sumsNInts (toInteger $ length arr) arr) ++ " which sums nInts" 
+getInteger n arr = do
+            line <- getLine
+            let x = read line :: Integer
+            getInteger (n-1) (arr ++ [x])
+
+sumsNInts :: Integer -> [Integer] -> Integer
+sumsNInts 0 _ = 0
+sumsNInts n (x:xs) = x + sumsNInts (n-1) xs
